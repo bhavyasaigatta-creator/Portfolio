d710f7989f70e2a255cba06d5bb334a89432a0ff
Bhavya Sai <bhavyasaigatta@gmail.com>
2026-05-04 22:29:39 +0530

    fix mongodb connection

--- server.js
+++ server.js
@@ -62,9 +62,12 @@ app.put("/projects/:id", async (req, res) => {
 });
 
 // ---------------- MONGODB ----------------
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("MongoDB Error:", err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("MongoDB Error:", err));
 
 // ---------------- RENDER FIX ----------------
 // IMPORTANT: Render provides dynamic PORT
